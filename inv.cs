eea3303ab458fe29f46ba83e715fd8ba60da00b2
Selçuk Ulusal <115923621+selcukulusal@users.noreply.github.com>
2026-05-06 18:27:05 -0700

    added pastel package

--- inv.cs
+++ inv.cs
@@ -9,28 +9,28 @@ namespace InventoryManagementSystem
             string username = "userA";
             int password = 12345;
 
-            Console.WriteLine("Welcome to Selcuk's CLI based Inventory Management System!");
+            Console.WriteLine("Welcome to Selcuk's CLI based Inventory Management System!"));
             Console.WriteLine();
 
             bool IsLoggedin = true;
 
             while (IsLoggedin)
             {
-                Console.Write("Please entry your username: ");
+                Console.Write("Please entry your username: ".Pastel(ConsoleColor.Blue));
                 string? inputUsername = (Console.ReadLine());
 
-                Console.Write("Please entry your password: ");
+                Console.Write("Please entry your password: ".Pastel(ConsoleColor.Blue));
                 int inputPassword = int.TryParse(Console.ReadLine(), out int parsedPassword) ? parsedPassword : 0;
                 Console.WriteLine();
 
                 if (inputUsername == username && inputPassword == password)
                 {
-                    Console.WriteLine($"Entry is Successful! Welcome {username}!");
+                    Console.WriteLine($"Entry is Successful! Welcome {username}!".Pastel(ConsoleColor.Green));
                     break;
                 }
                 else
                 {
-                    Console.WriteLine("Invalid username or password. Please try again.");
+                    Console.WriteLine("Invalid username or password. Please try again.".Pastel(ConsoleColor.Red));
 
                 }
 
